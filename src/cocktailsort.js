4b244e8b32f27976ac7c601cce3ab92dcd40a4ec
balajikamalesh <abalajikamalesh@gmail.com>
2023-12-19 18:50:50 +0530

    Update cocktailsort.js

--- src/cocktailsort.js
+++ src/cocktailsort.js
@@ -1,105 +1,69 @@
-async function cocktailSort(delay) {
-    let blocks = document.querySelectorAll(".block");
+const slider = document.getElementById("changeSize");
+const container = document.getElementById("data-container");
+const warningContainer = document.getElementById("warning");
+const resultContainer = document.querySelector("#result");
 
-    if(blocks.length > 250)
-      delay = delay/8;
-  
-      // disable menu
-      disableMenu();
-  
-      // reset results section
-      document.getElementById("result").innerHTML = '';
-      let result = document.getElementById("result");
-      let res = document.createElement('h3');
-  
-        // check array is sorted or not
-        var arr = [];
-        for(i=0;i<blocks.length;i++){
-          arr.push(Number(blocks[i].childNodes[0].innerHTML));
-        }
-  
-        if(isSorted(arr)){
-  
-            res.innerHTML = 'Array is already Sorted!!!';
-            result.appendChild(res);
-            
-            // enable menu
-            enableMenu();
-  
-        } else {
-  
-                res.innerHTML = 'Cocktail Sorting Started...';
-                result.appendChild(res);
-  
-                var startTime = performance.now();
+let dataset = [];
 
-                var swapped = true; 
-                var start = 0; 
-                var end = blocks.length - 1; 
-              
-                while (swapped) { 
-                    swapped = false; 
-            
-                    for (var i = start; i < end; i++) { 
-                        blocks[i].style.backgroundColor = "#FF4949";
-                        if (Number(blocks[i].childNodes[0].innerHTML) > Number(blocks[i+1].childNodes[0].innerHTML)) { 
-                            await new Promise(resolve =>
-                                setTimeout(() => {
-                                    resolve();
-                                }, 0.01*delay)
-                            );
-                            await swap(blocks[i], blocks[i + 1]); 
-                            blocks = document.querySelectorAll(".block");
-                            swapped = true; 
-                        } 
-                        blocks[i].style.backgroundColor = "#58B7FF";
-                    } 
-            
-                    if (!swapped) 
-                        break; 
-                    
-                    await new Promise(resolve =>
-                        setTimeout(() => {
-                            resolve();
-                        }, 0.01*delay)
-                    );
-            
-                    swapped = false; 
-            
-                    blocks[end].style.backgroundColor = "#13CE66";
-                    --end; 
-            
-                    for (var j = end - 1; j >= start; j--) { 
-                        blocks[j].style.backgroundColor = "#FF4949";
-                        if (Number(blocks[j].childNodes[0].innerHTML) > Number(blocks[j+1].childNodes[0].innerHTML)) { 
-                            await new Promise(resolve =>
-                                setTimeout(() => {
-                                    resolve();
-                                }, 0.01*delay)
-                            );
-                            await swap(blocks[j], blocks[j + 1]); 
-                            blocks = document.querySelectorAll(".block");
-                            swapped = true; 
-                        } 
-                        blocks[j+1].style.backgroundColor = "#58B7FF";
-                    } 
-            
-                    blocks[start].style.backgroundColor = "#13CE66";
-                    ++start; 
-                } 
-                blocks.forEach(function(el,index){
-                    blocks[blocks.length -1 - index].style.backgroundColor = "#13CE66";
-                })
+init();
 
-                
-                res.innerHTML = res.innerHTML + ` Sorting Complete!!`;
-                var endTime = performance.now();
-                let time = document.createElement('p');
-                time.innerHTML = `Time taken: ${((endTime-startTime)/1000).toFixed(4)} Sec `;
-                res.appendChild(time);
-  
-                // enable menu and remove highlight
-                enableMenu();
-  
-      }
-    }
+function init() {
+  generateBlocks(Number(slider.value));
+
+  slider.addEventListener("input", generateBlockUtil);
+}
+
+function generateBlockUtil() {
+  container.innerHTML = '';
+  resultContainer.innerHTML = '';
+  warningContainer.innerHTML = '';
+
+  if (slider.value > 150) {
+    showWarning("Dataset size is very high. Quick sort recommended!");
+  }
+
+  generateBlocks(Number(slider.value));
+}
+
+function generateBlocks(num = 45) {
+  dataset = Array.from({ length: num - 1 }, (_, i) => i + 1);
+  shuffleArray(dataset);
+
+  const fragment = document.createDocumentFragment();
+
+  dataset.forEach((value, i) => {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.setAttribute('block-value', value);
+
+    const heightMultiplier = calculateHeightMultiplier(num);
+    setBlockStyles(block, value, num, i, heightMultiplier);
+
+    fragment.append(block);
+  });
+
+  container.append(fragment);
+}
+
+function calculateHeightMultiplier(num) {
+  if (num < 40) return 7;
+  else if (num < 100) return 3;
+  else if (num < 200) return 1.35;
+  else if (num < 300) return 0.9;
+  else if (num < 400) return 0.6;
+  else return 0.5;
+}
+
+function setBlockStyles(block, value, num, index, heightMultiplier) {
+  block.style.height = `${value * heightMultiplier}px`;
+  block.style.width = `${900 / num}px`;
+  block.style.left = `${index * (900 / num)}px`;
+  block.style.marginLeft = '0.5px';
+  block.style.marginRight = '0.5px';
+}
+
+function showWarning(message) {
+  const res = document.createElement('h3');
+  res.innerHTML = `Warning: ${message}`;
+  warningContainer.appendChild(res);
+}
