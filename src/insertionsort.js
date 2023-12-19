aec99b07b19f8644c8831ba5dc02a2b104580e62
balajikamalesh <abalajikamalesh@gmail.com>
2023-12-19 18:51:56 +0530

    Update insertionsort.js

--- src/insertionsort.js
+++ src/insertionsort.js
@@ -1,72 +1,56 @@
 async function insertionSort(delay) {
-    let blocks = document.querySelectorAll(".block");
+  let blocks = document.querySelectorAll(".block");
 
-    if(blocks.length > 250)
-      delay = delay/8;
-  
-      // disable menu
-      disableMenu();
-      
-  
-      // reset results section
-      document.getElementById("result").innerHTML = '';
-      let result = document.getElementById("result");
-      let res = document.createElement('h3');
-  
-        // check array is sorted or not
-        var arr = [];
-        for(i=0;i<blocks.length;i++){
-          arr.push(Number(blocks[i].childNodes[0].innerHTML));
-        }
-  
-        if(isSorted(arr)){
-  
-            res.innerHTML = 'Array is already Sorted!!!';
-            result.appendChild(res);
-            
-            // enable menu
-            enableMenu();            
-  
-        } else {
-  
-                res.innerHTML = 'Insertion Sorting Started...';
-                result.appendChild(res);
-  
-                var start = performance.now();
+  if (blocks.length > 250) {
+    delay = delay / 8;
+  }
 
-                for (let i = 1; i < blocks.length; i++) {
-                    for (let j = i; j > 0 && Number(blocks[j].childNodes[0].innerHTML) < Number(blocks[j-1].childNodes[0].innerHTML); j--) {
-                        
-                        blocks[j].style.backgroundColor = "#FF4949";
-                        blocks[j-1].style.backgroundColor = "#FF4949";
-                        
-                        await new Promise(resolve =>
-                          setTimeout(() => {
-                            resolve();
-                          }, 2*delay)
-                        );
+  // disable menu
+  disableMenu();
 
-                        await swap(blocks[j], blocks[j - 1]);
-                        blocks = document.querySelectorAll(".block");
-                        
-                        blocks[j].style.backgroundColor = "#58B7FF";                        
-                        blocks[j-1].style.backgroundColor = "#58B7FF";
-                    }
-                }
+  // reset results section
+  const result = document.getElementById("result");
+  result.innerHTML = '';
 
-                for(let i=0;i<blocks.length;i++){
-                    blocks[i].style.backgroundColor = "#13CE66";
-                }
+  // check if the array is sorted
+  const arr = Array.from(blocks, block => getValue(block));
 
-                
-                res.innerHTML = res.innerHTML + ` Sorting Complete!!`;
-                var end = performance.now();
-                let time = document.createElement('p');
-                time.innerHTML = `Time taken: ${((end-start)/1000).toFixed(4)} Sec `;
-                res.appendChild(time);
-  
-                // enable menu and remove highlight
-                enableMenu();
-  
+  if (isSorted(arr)) {
+    const res = document.createElement('h3');
+    res.innerHTML = 'Array is already Sorted!!!';
+    result.appendChild(res);
+
+    // enable menu
+    enableMenu();
+  } else {
+    const res = document.createElement('h3');
+    res.innerHTML = 'Insertion Sorting Started...';
+    result.appendChild(res);
+
+    const start = performance.now();
+
+    for (let i = 1; i < blocks.length; i++) {
+      for (let j = i; j > 0 && getValue(blocks[j]) < getValue(blocks[j - 1]); j--) {
+        highlightBlocks(blocks[j], blocks[j - 1], "#FF4949");
+
+        await delayAnimation(2 * delay);
+
+        await swap(blocks[j], blocks[j - 1]);
+        blocks = document.querySelectorAll(".block");
+
+        highlightBlocks(blocks[j], blocks[j - 1], "#58B7FF");
       }
     }
+
+    highlightSortedBlocks(blocks);
+
+    res.innerHTML += ' Sorting Complete!!';
+    const end = performance.now();
+    const time = document.createElement('p');
+    time.innerHTML = `Time taken: ${((end - start) / 1000).toFixed(4)} Sec `;
+    res.appendChild(time);
+
+    // enable menu and remove highlight
+    enableMenu();
+  }
+}
