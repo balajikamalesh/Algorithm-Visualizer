d1f9735bf56eb578c0dd5a66c21bfd00c753735c
balajikamalesh <abalajikamalesh@gmail.com>
2023-12-19 18:50:28 +0530

    refactor bubblesort.js

--- src/bubblesort.js
+++ src/bubblesort.js
@@ -1,114 +1,76 @@
-var algos = document.getElementsByClassName("algorithmButton");
-var sortSpeed = document.getElementById("changeSpeed");
-var sortSpeedValue = 10;
+  const algos = document.querySelectorAll(".algorithmButton");
+  const sortSpeed = document.getElementById("changeSpeed");
+  let sortSpeedValue = 10;
 
-sortSpeed.addEventListener("input",function(){
-  sortSpeedValue = this.value;
-})
-
-for (var i = 0; i < algos.length; i++) {
-  algos[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("highlight");
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace(" highlight", "");
-    }
-    this.className += " highlight";
+  sortSpeed.addEventListener("input", function () {
+    sortSpeedValue = this.value;
   });
-}
 
-function swap(el1, el2) {
-    return new Promise(resolve => {
-      const style1 = window.getComputedStyle(el1);
-      const style2 = window.getComputedStyle(el2);
-  
-      const transform1 = style1.getPropertyValue("transform");
-      const transform2 = style2.getPropertyValue("transform");
-  
-      el1.style.transform = transform2;
-      el2.style.transform = transform1;
-  
-      var clonedElement1 = el1.cloneNode(true);
-      var clonedElement2 = el2.cloneNode(true);
-  
-      window.requestAnimationFrame(function() {
-        setTimeout(() => {
-          el1.replaceWith(clonedElement2);
-          el2.replaceWith(clonedElement1);
-          resolve();
-        }, 1000/(3*sortSpeedValue*sortSpeedValue));
-      });
+  algos.forEach(function (algo) {
+    algo.addEventListener("click", function () {
+      const current = document.querySelector(".highlight");
+      if (current) {
+        current.classList.remove("highlight");
+      }
+      this.classList.add("highlight");
     });
-  }
-  
-  async function bubbleSort(delay) {
+  });
 
+  async function bubbleSort(delay) {
     let blocks = document.querySelectorAll(".block");
 
-    if(blocks.length > 250)
-      delay = delay/8;
-    
-    // disable menu
+    if (blocks.length > 250) {
+      delay /= 8;
+    }
+
     disableMenu();
 
-    // reset results section
-    document.getElementById("result").innerHTML = '';
-    let result = document.getElementById("result");
-      let res = document.createElement('h3');
+    const result = document.getElementById("result");
+    result.innerHTML = "";
 
-      // check array is sorted or not
-    var arr = [];
-    for(i=0;i<blocks.length;i++){
-      arr.push(Number(blocks[i].childNodes[0].innerHTML));
-    }    
-    
-    if(isSorted(arr)){ 
+    const res = document.createElement("h3");
+    const arr = Array.from(blocks, (block) => Number(block.getAttribute("block-value")));
 
-      res.innerHTML = 'Array is already Sorted!!';
+    if (isSorted(arr)) {
+      res.innerHTML = "Array is already Sorted!!";
       result.appendChild(res);
-      
-      // enable menu
       enableMenu();
-    
-    } else { 
-      res.innerHTML = 'Bubble Sorting Started...';
+    } else {
+      res.innerHTML = "Bubble Sorting Started...";
       result.appendChild(res);
-      
-      var start = performance.now();
-      
+
+      const start = performance.now();
+
       for (let i = 0; i < blocks.length - 1; i += 1) {
         for (let j = 0; j < blocks.length - i - 1; j += 1) {
           blocks[j].style.backgroundColor = "#FF4949";
           blocks[j + 1].style.backgroundColor = "#FF4949";
-    
-          await new Promise(resolve =>
-            setTimeout(() => {
-              resolve();
-            }, 0.1*delay)
-          );
-    
-          const value1 = Number(blocks[j].childNodes[0].innerHTML);
-          const value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
-    
+
+          await new Promise((resolve) => setTimeout(resolve, 0.1 * delay));
+
+          const value1 = getValue(blocks[j]);
+          const value2 = getValue(blocks[j + 1]);
+
           if (value1 > value2) {
             await swap(blocks[j], blocks[j + 1]);
             blocks = document.querySelectorAll(".block");
           }
-    
+
           blocks[j].style.backgroundColor = "#58B7FF";
           blocks[j + 1].style.backgroundColor = "#58B7FF";
         }
-    
+
         blocks[blocks.length - i - 1].style.backgroundColor = "#13CE66";
       }
-      
+
       blocks[0].style.backgroundColor = "#13CE66";
-      res.innerHTML = res.innerHTML + ` Sorting Complete!!`;
-      var end = performance.now();
-      let time = document.createElement('p');
-      time.innerHTML = `Time taken: ${((end-start)/1000).toFixed(4)} Sec`;
+      res.innerHTML += " Sorting Complete!!";
+
+      const end = performance.now();
+      const time = document.createElement("p");
+      time.innerHTML = `Time taken: ${((end - start) / 1000).toFixed(4)} Sec`;
       res.appendChild(time);
 
-      // enable menu and remove highlight
       enableMenu();
     }
   }
